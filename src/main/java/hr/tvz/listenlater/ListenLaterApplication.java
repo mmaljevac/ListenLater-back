78ab77aa519d97de714cf85228331c907a704456
Matija Maljevac <mmaljevac@neos.hr>
2023-11-21 03:25:58 +0100

    initial

---
+++ src/main/java/hr/tvz/listenlater/ListenLaterApplication.java
@@ -0,0 +1,13 @@
+package hr.tvz.listenlater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListenLaterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ListenLaterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hr/tvz/listenlater/ListenLaterApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.tvz.listenlater;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListenLaterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
