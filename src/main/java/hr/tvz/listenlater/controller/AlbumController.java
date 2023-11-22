5f543a94a6744055cb0b4a21a662e32f83c5a525
Matija Maljevac <mmaljevac@neos.hr>
2023-11-22 11:27:59 +0100

    Add CrossOrigin

--- src/main/java/hr/tvz/listenlater/controller/AlbumController.java
+++ src/main/java/hr/tvz/listenlater/controller/AlbumController.java
@@ -12,6 +12,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/albums")
 @AllArgsConstructor
+@CrossOrigin(origins = "http://localhost:3000")
 public class AlbumController {
 
     private final AlbumService albumService;
